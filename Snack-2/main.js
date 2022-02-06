71d05eea5c35287a1f35bf04053c3a196fa91a12
Matteo Rossi <matteorossii6920@gmail.com>
2022-02-06 17:06:29 +0100

    Esercizio completato

--- Snack-2/main.js
+++ Snack-2/main.js
@@ -34,22 +34,18 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 } 
 
+const squadraFalli = []  
 
 for (let i = 0; i < squadre.length; i++) {
     squadre[i].punti = randomNumber(10, 60);
     squadre[i].falli = randomNumber(1, 15)
-}
-console.log(squadre)
 
-
-const squadraFalli = []  
-
-for (let i = 0; i < squadre.length; i++) {
     const {nome, falli} = squadre[i];
 
     squadraFalli.push({nome, falli})
 }
-    
+
+console.log(squadre)    
 console.log(squadraFalli)
 
 
